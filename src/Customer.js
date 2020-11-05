6d11245ab7743e08b741382dda7ab70f07fae975
Jordon Smith <jordon00smith@gmail.com>
2020-11-05 11:41:47 -0700

    Update Customer.js

--- src/Customer.js
+++ src/Customer.js
@@ -1,13 +1,48 @@
 class Customer {
-    constructor(username) {
-        this.username = username;
-        this.password = 'overlook2020';
+    constructor(username, name) {
+        this.username = username
+        this.name = name
+        this.password = 'overlook2020'
         if (username.includes('customer')) {
-            this.id = parseInt(username.slice(8));
+            this.id = parseInt(username.slice(8))
         } else {
-            this.id = 0;
+            this.id = 0
+        }
+        this.date = new Date().toDateString()
+        this.amountSpent = 0
+        this.presentBookings = []
+        this.futureBookings = []
+        this.pastBookings = []
+    }
+
+    confirmCustomerLogin(enteredPassword) {
+        if (enteredPassword === this.password) {
+            return 'valid credentials'
+        } else {
+            return 'Incorrect username or password. Please try again.';
         }
     }
-};
 
-export { Customer };
+    getCustomerBookings(bookings) {
+        return bookings.filter(booking => {
+            return booking.userID === this.id;
+        })
+    }
+
+    calculateAmountSpent(hotel, bookings) {
+        const customerBookings = this.getCustomerBookings(bookings)
+        hotel.rooms.forEach(room => {
+            customerBookings.forEach(booking => {
+                if (booking.roomNumber === room.number) {
+                    this.amountSpent += room.costPerNight
+                }
+            })
+        })
+    }
+
+    getCustomerBookings() {
+        
+    }
+}
+
+export { Customer }
