c162154d58b38966c4eac08f90155e3411e38fb0
Jordon Smith <jordon00smith@gmail.com>
2020-11-10 03:21:13 -0700

    Instantiate all data from API

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ let bookingData;
 let userData;
 let roomData;
 
-const mainPage = document.querySelector('main');
 const navBar = document.querySelector('nav');
+const mainPage = document.querySelector('main');
 const userLoginForm = document.querySelector('.user-login-form');
+
+function instantiateAllData(users, rooms, bookings) {
+    userData = users;
+    currentHotel = new Hotel(rooms);
+    bookingData = bookings;
+}
+
+function getAllData() {
+    const users  = apiData.fetchUserData();
+    const rooms = apiData.fetchRoomData();
+    const bookings = apiData.fetchBookingData();
+    Promise.all([users, rooms, bookings]).then(data => {
+      instantiateAllData(data[0], data[1], data[2]);
+    })
+}
