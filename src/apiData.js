6f007e02f6f204e55c599171f488f47f133e0556
Jordon Smith <jordon00smith@gmail.com>
2020-11-10 01:04:57 -0700

    Fetch room data

--- src/apiData.js
+++ src/apiData.js
@@ -6,5 +6,11 @@ export let apiData = {
         .catch(error => console.log(error.message));
     },
   
-    
+    fetchRoomData() {
+        return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+          .then(response => response.json())
+          .then(data => data.rooms)
+          .catch(error => console.log(error.message));
+      },
+  
 }
