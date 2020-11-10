47d5d96f8dc071ecd5138551aff3dd7b97dd9e43
Jordon Smith <jordon00smith@gmail.com>
2020-11-10 01:04:21 -0700

    Fetch user data

---
+++ src/apiData.js
@@ -0,0 +1,10 @@
+export let apiData = {
+    fetchUserData() {
+      return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+        .then(response => response.json())
+        .then(data => data.users)
+        .catch(error => console.log(error.message));
+    },
+  
+    
+}
