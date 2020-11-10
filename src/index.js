4b17e2e84a7dc78854071202ccf4e90c0355fa83
Jordon Smith <jordon00smith@gmail.com>
2020-11-10 03:11:07 -0700

    Estabilish global variables

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import Hotel from './Hotel';
 import { apiData } from './apiData'
 import './css/base.scss';
 
+let currentCustomer;
+let currentManager;
+let currentHotel;
+let bookingData;
+let userData;
+let roomData;
+
+const mainPage = document.querySelector('main');
+const navBar = document.querySelector('nav');
+const userLoginForm = document.querySelector('.user-login-form');
