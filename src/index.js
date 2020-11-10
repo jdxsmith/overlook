499436556daab95969ac2299fa47001d7221c3de
Jordon Smith <jordon00smith@gmail.com>
2020-11-10 03:05:17 -0700

    Import files into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+import Customer from './Customer';
+import Manager from './Manager';
+import Hotel from './Hotel';
+import { apiData } from './apiData'
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
